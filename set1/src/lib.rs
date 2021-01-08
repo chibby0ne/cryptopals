4240f110920849d23ac394b469825a2604fd21cd
Antonio Gutierrez <chibby0ne@gmail.com>
2021-01-08 19:58:25 +0100

    set1/challenge1: finish and pass challenge1
    The issue before was that I assumed the string had to be read as a
    string, which had characters representing hex values, which meant that
    each character would have a 8-bit binary representation, since they are
    all ASCII characters.
    
    Instead it is passed as a string but is one big number in hex
    representation, therefore each 'character' or digit, has a 4-bit binary
    representation.
    
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- set1/src/lib.rs
+++ set1/src/lib.rs
@@ -14,6 +14,21 @@ impl fmt::Display for InvalidHexCharFoundError {
 }
 impl Error for InvalidHexCharFoundError {}
 
+// Convert hex to 8-bit binary
+fn hex_to_binary(input: &str) -> Result<String, InvalidHexCharFoundError> {
+    let mut binary = String::new();
+    for b in input.bytes() {
+        let res = match b {
+            b'0'..=b'9' => Ok(format!("{:04b}", b - b'0')),
+            b'a'..=b'f' => Ok(format!("{:04b}", b - b'a' + 10)),
+            b'A'..=b'F' => Ok(format!("{:04b}", b - b'A'+ 10)),
+            _ => Err(InvalidHexCharFoundError { ch: char::from(b) }),
+        };
+        binary.push_str(&res?);
+    }
+    Ok(binary)
+}
+
 // Convert hex string to 8-bit binary
 fn hex_string_to_binary(input: &str) -> Result<String, InvalidHexCharFoundError> {
     let mut binary = String::new();
@@ -79,12 +94,42 @@ fn base64_from_string(input: &str) -> Result<String, InvalidHexCharFoundError> {
     Ok(result)
 }
 
+fn challenge1(input: &str) -> Result<String, InvalidHexCharFoundError> {
+    let mut result = String::new();
+    let binary_input = hex_to_binary(input)?;
+    dbg!(&binary_input);
+    let mut chars_in_block = 0;
+    for i in (0..binary_input.len()).step_by(6) {
+        match binary_input.get(i..i + 6) {
+            Some(slice) => {
+                dbg!(slice);
+                add_base64_char(&mut result, slice);
+                chars_in_block += 1;
+            },
+            None =>  {
+                add_base64_char(&mut result, binary_input.get(i..).unwrap());
+                chars_in_block += 1;
+                while chars_in_block != 4 {
+                    result.push('=');
+                    chars_in_block += 1;
+                }
+            },
+        };
+        if chars_in_block == 4 {
+            chars_in_block = 0;
+        }
+    }
+    Ok(result)
+}
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_hex_to_octal() {
+    fn test_hex_string_to_binary() {
         let res = hex_string_to_binary("2");
         assert!(res.is_ok());
         assert_eq!(res.unwrap(), String::from("00110010"));
@@ -127,8 +172,32 @@ mod tests {
     }
 
     #[test]
+    fn test_hex_to_binary_4() {
+        let res = hex_to_binary("4");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), String::from("0100"));
+    }
+
+    #[test]
+    fn test_hex_to_binary_49() {
+        let res = hex_to_binary("49");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), String::from("01001001"));
+    }
+
+    #[test]
+    fn test_hex_to_binary_492() {
+        let res = hex_to_binary("492");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), String::from("010010010010"));
+    }
+
+    #[test]
     fn test_challenge1() {
         let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         let expected_output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+        let res = challenge1(input);
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), expected_output)
     }
 }
