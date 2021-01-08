94cc9c3d48f130de539b0e4c7711657ea157aaf0
Antonio Gutierrez <chibby0ne@gmail.com>
2021-01-08 01:23:03 +0100

    set1/challenge1: wip with failing test
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- set1/src/lib.rs
+++ set1/src/lib.rs
@@ -1,31 +1,56 @@
-fn hex_to_octal(input: &str) -> Result<String, String>  {
-    let mut v: Vec<u8> = vec![];
-    for ch in input.chars().rev() {
-        let octal = match ch {
-            '0'..='7' => {
-                dbg!(ch);
-                dbg!(Ok(ch as u8 - b'0'))
-            },
-            '8'..='9' => Ok((ch as u8 - b'0') % 8),
-            'a'..='f' => Ok((ch as u8 - b'a' + 10) % 8),
-            'A'..='F' => Ok((ch as u8 - b'A' + 10) % 8),
-            _ => Err(format!("Not a valid hex character {}", ch)),
+#![feature(assoc_char_funcs)]
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+struct InvalidHexCharFoundError {
+    ch: char,
+}
+
+impl fmt::Display for InvalidHexCharFoundError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Invalid Hex Char: {}", self.ch)
+    }
+}
+impl Error for InvalidHexCharFoundError {}
+
+// Convert hex to 4-bit binary
+fn hex_to_binary(input: &str) -> Result<String, InvalidHexCharFoundError> {
+    let mut binary = String::new();
+    for b in input.bytes() {
+        let res = match b {
+            b'0'..=b'9' => Ok(format!("{:04b}", b - b'0')),
+            b'a'..=b'f' => Ok(format!("{:04b}", b - b'a')),
+            b'A'..=b'F' => Ok(format!("{:04b}", b - b'A')),
+            _ => Err(InvalidHexCharFoundError { ch: char::from(b) }),
         };
-        dbg!(&octal);
-        v.insert(0, octal?);
+        binary.push_str(&res?);
     }
-    dbg!(&v);
-    match String::from_utf8(v) {
-        Ok(s) => Ok(s),
-        Err(e) => Err(e.to_string()),
-    }
+    Ok(binary)
 }
 
 // Convert hex to base64
-fn challenge1(input: &str) -> &str {
-    let octal_input = hex_to_octal(input);
-
-    ""
+fn challenge1(input: &str) -> Result<String, InvalidHexCharFoundError> {
+    let mut result = String::new();
+    let binary_input = hex_to_binary(input)?;
+    for i in (0..binary_input.len()).step_by(6) {
+        match binary_input.get(i..i + 5) {
+            Some(slice) => {
+                if let Ok(value) = u32::from_str_radix(&format!("{}", slice), 2) {
+                    match value {
+                        0..=25 => result.push(char::from_u32(value + b'A' as u32).unwrap()),
+                        26..=51 => result.push(char::from_u32(value - 26 + b'a' as u32).unwrap()),
+                        52..=61 => result.push(char::from_u32(value - 52).unwrap()),
+                        62 => result.push('+'),
+                        63 => result.push('/'),
+                        _ => panic!("this is way bigger than 2**6"),
+                    }
+                }
+            }
+            None => result.push_str(&format!("{:=<6}", binary_input.get(i..).unwrap())),
+        };
+    }
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -34,14 +59,18 @@ mod tests {
 
     #[test]
     fn test_hex_to_octal() {
-        assert_eq!(hex_to_octal("2"), Ok(format!("2")));
+        let res = hex_to_binary("2");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), String::from("0010"));
     }
 
-    #[ignore]
+    // #[ignore]
     #[test]
     fn test_challenge1() {
-        let input =  r#"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"#;
+        let input = r#"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"#;
         let expected_output = r#"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"#;
-        assert_eq!(challenge1(input), expected_output)
+        let res = challenge1(input);
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), expected_output)
     }
 }
